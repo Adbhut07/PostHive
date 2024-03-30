e57f98f876314eaca13294d32450b92d72d7ad8b
Adbhut Satsangi <125047511+Adbhut07@users.noreply.github.com>
2024-03-30 15:42:44 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <h1 className='text-3xl text-red-400'>Hello</h1>
+    </>
+  )
+}
+
+export default App
